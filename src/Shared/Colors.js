73ae9facb5cb3284309422e62b0871a30eb68664
Nathalia Maia <devmaia@outlook.com>
2023-11-20 12:02:27 -0300

    🧿 Add: Added shared Color file.

---
+++ src/Shared/Colors.js
@@ -0,0 +1,14 @@
+export default{
+    black: "#020B19",
+    greenSolid: "#204144",
+    greenNeon: "#89FFDB",
+    white: "#D9D9D9",
+    whiteSolid: "#FFFFFF",
+    placeColor: "#93A29E",
+    textPlaceHolder: "#303E3A",
+    textRodape: "#8F8A8A",
+    button: "rgba(137, 255, 219, 0.51)",
+
+
+   
+}
