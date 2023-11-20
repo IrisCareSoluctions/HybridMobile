f6af05b834e6cb55e040f72c1fffc0e7054f7681
Nathalia Maia <devmaia@outlook.com>
2023-11-20 12:00:45 -0300

    🧿 Enhance: Improved default button and screen.

--- src/components/Buttons/ButtonDefault.js
+++ src/components/Buttons/ButtonDefault.js
@@ -1,30 +1,39 @@
 import React from 'react';
 import { Text, TouchableOpacity } from 'react-native';
+import Colors from '../../Shared/Colors';
 
-export default function ButtonDefault({ funcao, texto }) {
+export default function ButtonDefault({ onPress, title }) {
 
     return (
         <TouchableOpacity
-            onPress={funcao}
+            onPress={onPress}
+            activeOpacity={0.7}
             style={{
                 height: 50,
-                width: 200,
+                width: 150,
                 alignItems: 'center',
                 justifyContent: 'center',
-                backgroundColor: '#63C71F',
+                backgroundColor: "rgba(137, 255, 219, 0.51)",
+                borderColor: "rgba(137, 255, 219, 0.51)",
+                borderWidth: 2,
                 padding: 5,
-                borderRadius: 50,
-                marginTop: 40,
-                alignSelf: 'center'
+                margin: 20,
+                alignSelf: 'center',
+                borderRadius: 10,
+                fontSize: 18,
+                
+                
+                
             }}
         >
             <Text
                 style={{
-                    color: 'white',
+                    color: Colors.whiteSolid,
                     fontSize: 18,
                     textTransform: "uppercase",
+                    fontWeight: 'bold',
                 }}
-            >{texto}</Text>
+            >{title}</Text>
         </TouchableOpacity>
     );
 
